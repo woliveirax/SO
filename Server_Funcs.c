6be5c998156b2f8a5558d580a3c25c27dd400d1f
Unknown <wallace9655@gmail.com>
2017-11-11 18:11:00 +0000

    Estruturas finais coisas

--- Server_Funcs.c
+++ Server_Funcs.c
@@ -1,11 +1,40 @@
 #include "Serve_Funcs.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
 #define buffer_size 300
 
-
-void handleCommand(char * command)
+char * strUpper(char * str)
 {
+  char * a = malloc(sizeof(str));
   
+  for(;a != NULL;a++)
+    toupper(a);
+
+  return(a)
+}
+
+int strcmp_upperCase(char *str1, char *str2) //
+{
+  char *strA,*strB;
+
+
+
+
+  return strcmp(strUpper(strA),strUpper(strB));
+}
+
+void handleCommand(char * call)
+{
+  char *command, *saveptr;
+  command = strtok_r(call, " ", &saveptr);
+
+
+  strcmp(command,"add")
+
 }
 
 void console()
