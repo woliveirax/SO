484fc0a17d2b5da39429a9d3d759eef43ef1fead
FriasPTR <antonio.frias@outlook.pt>
2017-11-25 02:45:33 +0000

    alteraçao do comun_info PACKAGE
    inicio do protocolo de execuçao por parte do Bomberman e do INIMIGO

--- comun_info.h
+++ comun_info.h
@@ -9,7 +9,34 @@ typedef struct REQ_REG_USER
   username[MAX];
   password[MAX];
 }request_user;
-/////////////////////////////////////////////////
+//union onde irão estar todas as açoes que poderam ser feitas, bomberman, Client, user etc:...
+typedef union ACTION
+{
+// variavel para defenir possiveis tarefas do bomberman;
+  //  valor  assignment      Tecla
+  // B = 1   NORTH           up key
+  // B = 2   EAST            left key
+  // B = 3   SOUTH           low key
+  // B = 4   WEAST           right
+  // B = 5   bomb nuke       C key
+  // B = 6   Bomb granede    D key
+  // B = 7   jump            SPACE key
+  int MOV_Bomberman;
+// variavel para defenir tarefas do ENEMY;
+  // value   assignment      key
+  // E = 1   NORTH           up key
+  // E = 2   EAST            left key
+  // E = 3   SOUTH           low key
+  // E = 4   WEAST           right
+  int MOV_Enemy;
+// extrutura que irá servir para enviar os dados do cliente para efetuar o login;
+  request_user request_user;
+}action;
+//estrutura pacote a enviar com as enumeras açoes que podem existir
+typedef struct Package
+{
+  int type;
+  action action;
 
-
+}pack;
 #endif /* COMUN_INFO_H */
