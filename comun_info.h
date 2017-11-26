80b5ca4fed26d5404522568321494bfc5fe88a34
Unknown <wallace9655@gmail.com>
2017-11-26 21:19:37 +0000

    Server Exit 1.0

--- comun_info.h
+++ comun_info.h
@@ -21,69 +21,40 @@
 
 
 #define USER_COM      0 //Client connection
-
 #define USER_AUTH     1 //User authentication
-
 #define USER_REQUEST  2 //User Request Cred
-
 #define USER_ACTION   3 //Movement
-
 #define USER_EXIT     4 //Client Exit
 
 
-
 #define SERVER_KICK           20
-
 #define SERVER_AUTH           21
-
 #define SERVER_REQ_RESP       22
-
 #define SERVER_SEND_GAME_INFO 23
-
-//#define SERVER_KILL_CLI       24
-
 
 
 #define USER_LOGIN_ACCEPTED      0
-
 #define USER_DOESNT_EXIST       -2
-
 #define USER_LOGIN_WRONG_PASS   -3
-
 #define USER_ALREADY_IN         -4
-
 #define SERVER_FULL             -5
-
 
 
 //Estrutura usada para ser enviada pelo cliente para ser verificado o seu login
 
 typedef struct REQ_REG_USER
-
 {
-
   int PID;
-
   char username[MAX];
-
   char password[MAX];
-
-
-
 }Login;
 
 // estrutura que será enviada com o request para o servidor adicionar o novo utilizador;
 
 typedef struct MSG_REQ_REG_USER
-
 {
-
   int type;
-
   Login login;
-
-
-
 }MSG_Login;
 
 
--- server/server_interface.c
+++ server/server_interface.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
   ClientsData data;
   data.nClients = 0;
-  
+
   pipeMain(&data);
 
   //console();
